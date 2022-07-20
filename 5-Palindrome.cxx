ddf9b20c824873cce6e9dcc77af578cab2d3a1a3
Gabrielmvilla <gabrielmvilla@gmail.com>
2022-07-20 15:54:17 -0300

    Configuring git

--- 5-Palindrome.cxx
+++ 5-Palindrome.cxx
@@ -1,4 +1,7 @@
 /*
+ * This is a program/exercice made to work with strings, checking if a
+ * sentence, entered by a user, is a palindrome.
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -8,9 +11,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * This is a program/exercice made to work with strings, checking if a
- * sentence, entered by a user, is a palindrome.
  * 
  */
 
